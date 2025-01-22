1a044f7beda107ed7846516fc189096a29952b09
MyoungJinSeo <sih5673@gmail.com>
2025-01-22 19:23:43 +0900

    문자열을 정수로 바꾸기 - Lv1

---
+++ src/Programmers/M22.java
@@ -0,0 +1,14 @@
+package Programmers;
+
+public class M22 {
+    public int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        M22 m22 = new M22();
+        System.out.println(m22.solution("1234"));
+    }
+}
