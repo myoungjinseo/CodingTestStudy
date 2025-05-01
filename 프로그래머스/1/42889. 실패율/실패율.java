60c16fa17e29812d22a569ee9b363785fac41cc6
MyoungJin Seo <80959635+myoungjinseo@users.noreply.github.com>
2025-05-01 23:36:28 +0900

    [level 1] Title: 실패율, Time: 7.88 ms, Memory: 92 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n, int[] stages) {
+        int [] result = new int [n + 2];
+		for (int i : stages){
+			result[i]++;
+		}
+		HashMap<Integer, Double> fails = new HashMap<>();
+		double r = stages.length;
+		for(int i = 1; i <= n; i++){
+			if(result[i] == 0){
+				fails.put(i,0.0);
+			} else {
+				fails.put(i,result[i] / r);
+				r -= result[i];
+			}	
+		}
+		return fails.entrySet().stream().sorted((o1, o2) -> 
+		o1.getValue().equals(o2.getValue()) ? Integer.compare(o1.getKey(),
+		o2.getKey()) : Double.compare(o2.getValue(), 
+		o1.getValue())).mapToInt(HashMap.Entry::getKey).toArray();
+    }
+}
