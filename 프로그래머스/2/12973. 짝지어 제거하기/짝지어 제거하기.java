ba6a6a2101abe1fe00aac5b2191ec0e3d311b387
MyoungJin Seo <80959635+myoungjinseo@users.noreply.github.com>
2025-05-05 20:47:55 +0900

    [level 2] Title: 짝지어 제거하기, Time: 53.19 ms, Memory: 64.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution
+{
+    public int solution(String s){
+        int answer = 0;
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(!stack.isEmpty() && stack.peek() == c){
+                stack.pop();
+            } else{
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty() ? 1 : 0;
+    }
+}
