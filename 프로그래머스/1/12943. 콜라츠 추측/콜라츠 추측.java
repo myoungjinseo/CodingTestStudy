7f9a341911adf9c2fac29d30e91fdf95858c4ea2
MyoungJin Seo <80959635+myoungjinseo@users.noreply.github.com>
2025-05-09 22:02:55 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        while(num != 1){
+            answer++;
+            if(num % 2 == 1){
+                num = num * 3 + 1;
+            } else{
+                num /= 2;
+                
+            }
+            if(answer >= 500){
+                answer = - 1;
+                break;
+            }
+        }
+        return answer;
+    }
+}
