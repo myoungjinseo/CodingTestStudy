3c6d4964a651e591b0f1ff5c47424a4df5f3f2df
MyoungJin Seo <80959635+myoungjinseo@users.noreply.github.com>
2025-06-20 21:00:35 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 59.05 ms, Memory: 134 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        char[] arr = {'수','박'};
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++){
+            if(i % 2 == 0){
+                answer += arr[0];
+            } else{
+                answer += arr[1];
+            }
+        }
+        return answer;
+    }
+}
