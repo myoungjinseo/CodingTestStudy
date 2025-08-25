b376344410dad0052249bfcd4472278366f7289e
MyoungJin Seo <80959635+myoungjinseo@users.noreply.github.com>
2025-08-25 09:57:47 +0900

    [level 2] Title: 주식가격, Time: 21.96 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,17 +1,16 @@
-import java.util.*;
 class Solution {
-    public static int[] solution(int[] a){
-        int n = a.length;
-        int [] answer = new int[n];
-        int i,j;
-        for(i=0;i<n;i++){
-            for(j=i+1;j<n;j++){
-                answer[i]++;
-                if(a[i] > a[j]){
-                    break;
-                }
-            }
-        }
-        return answer;
+    public int[] solution(int[] prices) {
+        int n = prices.length;  
+        int [] answer = new int[n];  
+        int i,j;  
+        for(i=0;i<n;i++){  
+            for(j=i+1;j<n;j++){  
+                answer[i]++;  
+                if(prices[i] > prices[j]){  
+                    break;  
+                }  
+            }  
+        }  
+        return answer;  
     }
 }
