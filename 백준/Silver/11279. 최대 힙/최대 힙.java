7825daa8db57d4b43548a1e2db9e86f116155605
MyoungJin Seo <80959635+myoungjinseo@users.noreply.github.com>
2025-09-09 10:44:39 +0900

    [Silver II] Title: 최대 힙, Time: 924 ms, Memory: 37728 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        PriorityQueue<Integer> heap = new PriorityQueue<>(Collections.reverseOrder());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            if (a == 0) {
+                if (heap.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(heap.poll());
+                }
+            } else {
+                heap.add(a);
+            }
+        }
+    }
+}
