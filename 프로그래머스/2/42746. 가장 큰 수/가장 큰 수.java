889335ad39d8a1b4b79569b65fa5a6901698f72c
MyoungJin Seo <80959635+myoungjinseo@users.noreply.github.com>
2025-10-01 10:49:49 +0900

    [level 2] Title: 가장 큰 수, Time: 227.79 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,29 +1,19 @@
 import java.util.Arrays;
-import java.util.Comparator;
 
 class Solution {
     public String solution(int[] numbers) {
-        String[] data = Arrays
-            .stream(numbers)
-            .mapToObj(String::valueOf)
-            .toArray(String[]::new);
-
-    
-        Arrays.sort(data, (a, b) -> {
-            String combine1 = a + b;
-            String combine2 = b + a;
-            return combine2.compareTo(combine1); 
-        });
-
-    
-        StringBuilder sb = new StringBuilder();
-        for (String num : data) {
-            sb.append(num);
+        String[] answer = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            answer[i] = String.valueOf(numbers[i]);
         }
 
-    
-        if (sb.charAt(0) == '0') {
-            return "0";
+        Arrays.sort(answer, (a, b) -> (b + a).compareTo(a + b));
+
+        if (answer[0].equals("0")) return "0";
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < answer.length; i++) {
+            sb.append(answer[i]);
         }
 
         return sb.toString();
