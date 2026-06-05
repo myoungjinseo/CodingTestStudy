bfab9080707bed4daf30f03577677615d414d2c7
myoungjinseo <sih5673@gmail.com>
2026-06-05 23:28:14 +0900

    feat: 스택

---
+++ src/M2_DFSandBFS/M1.py
@@ -0,0 +1,16 @@
+# 스택
+
+stack = []
+
+stack.append(5) # 삽입
+stack.append(2)
+stack.append(3)
+stack.append(7)
+stack.pop()
+stack.append(1)
+stack.append(4)
+stack.pop() # 삭제
+
+print(stack) # 최하단 원소부터 출력
+print(stack[::-1]) # 최상단 원소부터 출력
+
